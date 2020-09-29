ac5e4eab0f2909968dcd49d6973f6dfd901e5aef
Eric Sullivan <eric.sullivan@maine.edu>
2020-09-29 11:06:07 -0400

    Fixed formating of the main C file

--- Calculator/Core/Src/stm32l4xx_it.c
+++ Calculator/Core/Src/stm32l4xx_it.c
@@ -197,6 +197,19 @@ void SysTick_Handler(void)
 /* please refer to the startup file (startup_stm32l4xx.s).                    */
 /******************************************************************************/
 
+/**
+  * @brief This function handles FPU global interrupt.
+  */
+void FPU_IRQHandler(void)
+{
+  /* USER CODE BEGIN FPU_IRQn 0 */
+
+  /* USER CODE END FPU_IRQn 0 */
+  /* USER CODE BEGIN FPU_IRQn 1 */
+
+  /* USER CODE END FPU_IRQn 1 */
+}
+
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
